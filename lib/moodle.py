314de9be880b686498e7724abd03b8728360e02c
ysh <mysh212@gmail.com>
2025-03-19 21:21:35 +0800

    Init commit for version 2.0
     - [RENEW] renew the whole project

--- lib/moodle.py
+++ lib/moodle.py
@@ -1,77 +1,199 @@
 from core.general import *
+
+import json
+from matplotlib import pyplot as plt
+
 import requests
 from bs4 import BeautifulSoup as bs
 
-url = 'https://moodle.ncku.edu.tw'
-token = read_from_file('token.debug.tmp').strip()
+import cv2
+import pytesseract as pt
 
-def get_courses(token: str) -> list:
-    html = requests.get(url, cookies = {'MoodleSession': token})
-    write_to_file('homepage.html', html.text)
+headers = dict([i.split(': ') for i in '''Host: moodle.ncku.edu.tw
+# Content-Length: 100
+Cache-Control: max-age=0
+Sec-Ch-Ua: "Chromium";v="131", "Not_A Brand";v="24"
+Sec-Ch-Ua-Mobile: ?0
+Sec-Ch-Ua-Platform: "Windows"
+Accept-Language: zh-TW,zh;q=0.9
+Origin: https://moodle.ncku.edu.tw
+Content-Type: application/x-www-form-urlencoded
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.86 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'''.split('\n') if not i.startswith('#')])
 
-    soup = bs(html.text, 'html.parser')
-    menu = soup.find('div', class_ = 'card-text content mt-3')
+root = 'https://moodle.ncku.edu.tw'
 
-    #print(menu.find_all('ul')[0].get('class'))
-    menu = [i for i in menu.find_all('ul') if 'unlist' not in i.get('class', [])]
+class moodle:
+    def __init__(self, username, password):
+        self.soup = ''
+        self.username = username
+        self.password = password
 
-    courses = [[[j.text, j.get('href', 'None')] for j in i.find_all('a')] for i in menu]
-    # for i in courses:
-    #     for j in i:
-    #         print(j)
-    #     print('')
+    def get_login(self):
+        url = f'{root}/login/index.php'
+        self.req = requests.get(url, headers = headers)
+        self.html = self.req.text
+        self.soup = bs(self.html, 'html.parser')
+        return self.req, self.soup
 
-    return courses
+    def get_token(self) -> str:
+        soup = self.soup
 
-def get_course_content(url: str):
-    html = requests.get(url, cookies = {'MoodleSession': token})
-    write_to_file('tmp.html', html.text)
+        self.login_token = soup.select_one('input[name=logintoken]')['value']
+        debug(self.login_token)
+        return self.login_token
 
-    soup = bs(html.text, 'html.parser')
-    pre = soup.find_all('div', class_ = 'content')
+    def get_session(self) -> str:
+        req = self.req
+
+        cookies = dict([i.split('=') for i in req.headers['Set-Cookie'].split('; ')])
+
+        self.session = cookies['MoodleSession']
+        debug(self.session)
+        return self.session
+
+    def get_login_key(self) -> str:
+        req = self.req
+        html = req.text
+        key = [i for i in html.split('\n') if i.find('M.cfg') != -1][0]
+        key = key[key.find('{') + 1:key.find('}')]
+        key = dict([[j[1:-1] for j in i.split(':', 1)] for i in key.split(',')])
+        key = key['sesskey']
+        debug(key)
+        self.key = key
+        return key
     
-    # error([i.text for i in soup.find_all(class_ = 'assign')])
-    # debug([len(pre)])
+    def send_junk(self):
+        session = self.session
+        junk = requests.post(f'{root}/lib/ajax/service.php?sesskey={self.key}&info=core_fetch_notifications', cookies = {
+            'MoodleSession': session
+        }, data = '''[{"index":0,"methodname":"core_fetch_notifications","args":{"contextid":1}}]''', headers = headers)
+        debug(json.loads(junk.text))
+    
+    def get_captcha(self):
+        soup = self.soup
+        session = self.session
+        
+        captcha = soup.find('img', id = 'imgcode')['src']
+        debug(captcha)
+        open('captcha.png', 'wb').write(requests.get(captcha, cookies = {
+            'MoodleSession': session
+        }).content)
+        self.img = cv2.imread('captcha.png')
+        return cv2.imread('captcha.png')
+    
+    def parse_img(self):
+        img = self.img
+        xx = [0,1,0,-1,1,-1,1,-1]
+        yy = [1,0,-1,0,1,1,-1,-1]
 
-    ans = []
+        white = img[1][1]
+        nimg = img.copy()
+        for i in range(img.shape[0]):
+            for j in range(img.shape[1]):
+                ans = 0
+                for k in range(8):
+                    nx = i + xx[k]
+                    ny = j + yy[k]
+                    if nx < 0 or ny < 0 or nx >= img.shape[0] or ny >= img.shape[1]: continue;
+                    if (img[nx][ny] == img[i][j]).all(): ans = ans + 1
+                if ans >= 2:
+                    continue
+                nimg[i][j] = white
 
-    for i in pre:
+        self.nimg = nimg[1:nimg.shape[0] - 1, 1:nimg.shape[1] - 1]
+        return self.nimg
+    
+    def get_code(self):
+        img, nimg = self.img, self.nimg
+
+        pt.pytesseract.tesseract_cmd = r'C:\Users\ysh00\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+        def get_code(i) -> str:
+            ans = pt.image_to_string(i, config = 'digits')
+            ans = ''.join([i for i in ans if i.isdigit()])
+            return ans
+
+        self.code = [get_code(i) for i in [img, nimg]]
+
+        debug(self.code)
+
+        return self.code
+
+        # ans
+    
+    def login(self):
+        username = self.username
+        password = self.password
+        code = self.code[1]
+        login_token = self.login_token
+        session = self.session
+
+        self.pre = requests.post(f'{root}/login/index.php', data = {
+            'anchor': '',
+            'logintoken': login_token,
+            'username': username,
+            'password': password,
+            'vcode': code
+        }, cookies = {
+            'MoodleSession': session
+        }, headers = headers, allow_redirects = False)
+        del self.password
         try:
-            title, url = (lambda x: [x.text, x.find('a').get('href', '')])(i.find('h3', class_ = 'sectionname'))
-            host = (i.find_all('li'))[1:]
-            # debug((title, url))
-
-            memo = i.find_all(dir = 'ltr')
-            # error(memo)
-            
-            contents = [{'text': i.find('a').text, 'url': i.find('a').get('href',''), 'html': i, 'ishomework': 'assign' in i.get('class', [])} for i in host if i.find('a') is not None]
-
-            contents += [{'text': i.text, 'url': '', 'html': i, 'ishomework': False} for i in memo]
-            ans.append({'title': title, 'url': url, 'data': contents})
-            # info(i.find_all('a'))
+            self.session = dict([i.split('=', 1) for i in self.pre.headers['Set-Cookie'].split('; ')])['MoodleSession']
+            debug(self.session)
+            # now = requests.get(f'{root}', cookies = {
+            #     'MoodleSession': session
+            # }, headers = headers).text
         except:
+            warning('Ignoring new token')
             pass
-    return ans
+        return self.pre
+    
+    def try_check_login(self):
+        pre = self.pre
+        session = self.session
 
-def view_course_content(x):
-    for i in x:
-        warning([i['title'], i['url']])
-        for j in i.get('data'):
-            p = info
-            if j['ishomework']: p = error
-            p([j['text'], j['url']])
+        self.pre = requests.get(pre.headers['Location'], cookies = {
+            'MoodleSession': session
+        }, headers = headers)
+        return self.pre
+    
+    def check_login(self):
+        pre, session = self.pre, self.session
 
-def get_all_contents():
-    ans = []
-
-    pre = get_courses(token)[0]
-    now = 1
-    for i in pre:
-        info(f'Getting Data for course {i[0]} [{now} / {len(pre)}]', end = '\r')
-        # for _ in range(100): print(' ', end = '');
-        ans.append({'title': i[0], 'url': i[1], 'id': i[1][i[1].find('id=') + 3:], 'data': get_course_content(i[1])})
-        now = now + 1
-    return ans
-
-def main():
-    return get_all_contents()
+        ns = bs(pre.text, 'html.parser')
+        if len(ns.find_all('a', id = 'loginerrormessage')) >= 1:
+            error(ns.find('a', id = 'loginerrormessage').text)
+            return False
+        else:
+            info('========== Final Token ==========')
+            info(f'{session:^33}')
+        return True
+    
+    def auto(self):
+        self.get_login()
+        self.get_token()
+        self.get_session()
+        self.get_login_key()
+        self.send_junk()
+        self.get_captcha()
+        self.parse_img()
+        self.get_code()
+        self.login()
+        self.try_check_login()
+        return self.check_login()
+    
+if __name__ == '__main__':
+    t = moodle()
+    t.get_login()
+    t.get_token()
+    t.get_session()
+    t.get_login_key()
+    t.send_junk()
+    t.get_captcha()
+    t.parse_img()
+    t.get_code()
+    t.login()
+    t.try_check_login()
+    info(t.check_login())
--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 from core.general import *
-from lib.moodle import get_all_contents
-from lib.diff import main
-import lib.io
-import json
+import lib.moodle as moodle
 
-f = get_all_contents()
-lib.io.save(f)
+md = moodle.moodle(*read_from_file('.env').split())
 
-# for i in f:
-#     info([i['title'], i['url'], i['id']])
-#     for k in i['data']:
-#         warning((k['title'], k['url']))
-#         for j in k['data']:
-#             info([j['text'], j['ishomework']])
-#             # debug(j.keys())
-#     input()
-
-ans = lib.diff.main(lib.io.translate(json.loads(read_from_file('last.json.bkp'))), lib.io.translate(lib.io.get()))
-for i in ans:
-    warning(i)
+md.auto()
+info(md.session)
